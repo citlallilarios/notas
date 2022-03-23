7a3cf58c7464211655847f1c0f31aae49ebe411b
citlalli larios huerta <isc18350321@gmail.com>
2022-03-23 17:07:53 -0600

    Input controlado

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+const App = ()=> {
+  // hook -> use.....
+  //state -> useState
 
-function App() {
+  const [state, setState] = useState("");
+
+  const handleInputChage = (event) => {
+   setState(event.target.value);
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>notas</h3>
+        <label>
+           Input de prueba 
+        <input 
+        id="pruebaID" 
+        name="prueba" 
+        type="text" 
+        onChange={handleInputChage}
+        value={state}
+         />
+         </label>
     </div>
   );
 }
