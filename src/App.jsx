d7768270d754b462f596242c425773c0d0893a14
citlalli larios huerta <isc18350321@gmail.com>
2022-05-03 15:12:48 -0500

    guardar datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,106 @@
-import {useState} from 'react'
-const App = ()=> {
-  // hook -> use.....
-  //state -> useState
+import { useState } from "react";
 
-  const [inputsState, setInputsState] = useState({
-    title: "",
-    date: "",
-    note: ""
-  });
-  
+const App = () => {
 
-  const handleInputChange = (event) => {
-   setInputsState({...inputsState, [event.target.name]: event.target.value });
-  };
-  const handleClickr = (event) => {
-    setInputsState({title: "",
-    date: "",
-    note: ""});
+// hook -> use .....
+//state -> useState
+
+const[inputsState, setInputsState] = useState({
+  title:"",
+   date:"",
+   note:""
+});
+  const handleInputChange = (event) => { 
+   setInputsState({...inputsState,[event.target.name]:event.target.value});
    };
-  
+
+const handleClickLimpiar = (event) =>{ 
+  setInputsState({title:"",
+  date:"",
+  note:""});
+};
+
+let notas = JSON.parse(localStorage.getItem("notas")) || [];
+const handleClickGuardar = () => {
+  notas.push(inputsState);
+  localStorage.setItem("notas", JSON.stringify(notas));
+  handleClickLimpiar();
+};
+
 
   return (
-    <div className="App container">
-      <div classNmae="row">
-        <div className="col">
-          <h3>Listas</h3>
-        </div>
+     <div className="App container">
+     <div className="row">
+       <div className="col">
+         <h3>lista</h3>
+         <ul>
+          {notas.map((nota) => {
+            return (
+              <li>
+                {nota.title} ({nota.date})
+              </li>
+            );
+            })}
+         </ul>
+       </div>
+     <div className="col">
+     <h3>Notas</h3>
+     <label className="mb-2" style={{width:"100%"}}></label>
+     <label style={{width:"100%"}}>Titulo
+     <input 
+     id="title" 
+     name="title" 
+     type="text" 
+     onChange={handleInputChange}
+     value={inputsState.title}
+     style={{width:"100%"}}
+      />
+      </label>
+      <br />
+      <label className="mb-2" style={{width:"100%"}}>Fecha
+     
+     <input 
+     id="date" 
+     name="date" 
+     type="text" 
+     onChange={handleInputChange}
+     value={inputsState.date}
+     style={{width:"100%"}}
+      />
+      </label >
+      <br />
+      <label style={{width:"100%"}}>Nota
+     
+     <input 
+     id="note" 
+     name="note" 
+     type="text" 
+     onChange={handleInputChange}
+     value={inputsState.note}
+     style={{width:"100%"}}
+      />
+      </label>
+      <hr />
+      <div className="row">
+      <span className="col">
+      <button className="btn btn-primary me-2" 
+      onClick={handleClickLimpiar} 
+      style={{width:"100%"}}
+      >limpiar</button>
+      </span>
+      <span className="col">
+        <button
+        type="button"
+        className="btn btn-primary"
+        onClick={handleClickGuardar}
+        style={{whith:"100%"}}>Guardar</button>
+      </span>
+      </div>
+      </div>
+      </div>
       
-      <div className="col">
-        <label className="mb-2">
-        </label>
-      
-        <h3>Nota</h3>
-        <label>
-          Titulo
-        <input 
-        id="title" 
-        name="title" 
-        type="text" 
-        onChange={handleInputChange}
-        value={inputsState.title}
-         />
-         </label>
-        <br />
-      
-         <label className="mb-2"> 
-          Fecha
-        <input 
-        id="date" 
-        name="date" 
-        type="text" 
-        onChange={handleInputChange}
-        value={inputsState.date}
-         />
-         </label>
-         <br />
-         <label>
-           Notas
-        <input 
-        id="note" 
-        name="note" 
-        type="text" 
-        onChange={handleInputChange}
-        
-        value={inputsState.note}
-         />
-         </label>
-         </div>
-         </div>
-         <hr />
-         <div className="row">
-         <button className="btn btn-primary me-2" onClick={ handleClickr}>Limpiar </button> </div>
     </div>
   );
-}
+};
 
 export default App;
